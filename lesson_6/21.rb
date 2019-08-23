86432d8f826228f985587a497a8ed40d45dd50b1
Chijioke Onyekwelu <coonyekwelu@gmail.com>
2019-08-23 07:14:34 -0700

    21 logic

--- lesson_6/21.rb
+++ lesson_6/21.rb
@@ -31,6 +31,7 @@ smaller number wins
 
 1. Initialize deck
 2. Deal cards to player and dealer
+update deck
 3. Player turn: hit or stay
   - repeat until bust or "stay"
 4. If player bust, dealer wins.
@@ -38,3 +39,34 @@ smaller number wins
   - repeat until total >= 17
 6. If dealer bust, player wins.
 7. Compare cards and declare winner.
+
+
+# data structure for hands should be array
+loop do
+  initialize_deck
+  initialize player_hand
+  initialize dealer_hand
+
+  prompt "Dealer has: #{} and unknown card"
+  prompt "You have: 2 and 8"
+
+  loop do
+    prompt "Enter 'h' to hit or 's' to stay:"
+    use conditional to update player hand
+    break if player_bust? or stay?#loop will terminate
+  end
+
+  break if player_bust?
+
+  loop do
+    dealer move (has conditional logic based on current total)
+    break if dealer_bust? or dealer_total >= 17#loop will terminate
+  end
+
+  break if dealer_bust?
+
+  check_winner(player_total, dealer_total)
+  break
+end
+
+declare_winner(player_total, dealer_total)
