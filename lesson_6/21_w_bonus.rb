e1601f046ae4a29b796080ea51ab92999494694e
Chijioke Onyekwelu <coonyekwelu@gmail.com>
2019-08-25 15:21:55 -0700

    21 w bonus

--- lesson_6/21_w_bonus.rb
+++ lesson_6/21_w_bonus.rb
@@ -1,31 +1,3 @@
-#
-#
-# two players
-# - dealer
-# - player
-#
-# initial dealing of two cards to each []
-#
-# store card values as constant
-# ace has two values depending on total of cards in hand
-#
-# check if total <= 11
-# draw another card
-#
-# dealer to show one card
-#
-# player choice [hit or stay]
-# if hit deal another card to player
-#   check total, if greater than 21 bust
-# turn over
-#
-# dealer choose to hit if total <= 17 else stay
-#
-# if dealer stay? and player stay? compare 21 - dealer to 21 - player
-#
-# smaller number wins
-#
-#
 # 1. Initialize deck
 # 2. Deal cards to player and dealer
 # update deck
