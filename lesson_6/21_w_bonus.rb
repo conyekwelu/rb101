984be651269c7b18b78fbdde1c7a2590b2a3a7d6
Chijioke Onyekwelu <coonyekwelu@gmail.com>
2019-08-30 21:32:44 -0700

    updated game logic

--- lesson_6/21_w_bonus.rb
+++ lesson_6/21_w_bonus.rb
@@ -9,16 +9,22 @@
 # 6. If dealer bust, player wins.
 # 7. Compare cards and declare winner.
 
-SCORE = {"A" => 11, "J" => 10, "Q" =>10, "K" =>10, "2" => 2, "3" => 3, "4" => 4,
-        "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, "10" => 10}
+SCORE = { "A" => 11, "J" => 10, "Q" => 10, "K" => 10, "2" => 2, "3" => 3,
+          "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9,
+          "10" => 10 }
+
+SUITS = ['H', 'D', 'S', 'C']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 BUST_POINT = 21
 DEALER_HEDGE = 17
 
-deck = [["H", "A"], ["H", "J"], ["H", "Q"], ["H", "K"], ["H", "2"], ["H", "3"], ["H", "4"], ["H", "5"], ["H", "6"], ["H", "7"], ["H", "8"], ["H", "9"], ["H", "10"], ["D", "A"], ["D", "J"], ["D", "Q"], ["D", "K"], ["D", "2"], ["D", "3"], ["D", "4"], ["D", "5"], ["D", "6"], ["D", "7"], ["D", "8"], ["D", "9"], ["D", "10"], ["C", "A"], ["C", "J"], ["C", "Q"], ["C", "K"], ["C", "2"], ["C", "3"], ["C", "4"], ["C", "5"], ["C", "6"], ["C", "7"], ["C", "8"], ["C", "9"], ["C", "10"], ["S", "A"], ["S", "J"], ["S", "Q"], ["S", "K"], ["S", "2"], ["S", "3"], ["S", "4"], ["S", "5"], ["S", "6"], ["S", "7"], ["S", "8"], ["S", "9"], ["S", "10"]]
+def initialize_deck
+  SUITS.product(VALUES).shuffle
+end
 
 def update_total(plyr_hand)
-  values = plyr_hand.map{ |card| card[1] }
+  values = plyr_hand.map { |card| card[1] }
   sum = 0
 
   values.each do |value|
@@ -33,7 +39,7 @@ def update_total(plyr_hand)
 end
 
 def update_deck_draw(array, draw)
-  array.select!{|e| !draw.include?(e)}
+  array.select! { |e| !draw.include?(e) }
 end
 
 def busted?(plyr_tot)
@@ -41,30 +47,67 @@ def busted?(plyr_tot)
 end
 
 def check_winner(player_tot, dealer_tot)
-  if player_tot > BUST_POINT || (dealer_tot < BUST_POINT && dealer_tot > player_tot)
-    return "Dealer"
-  elsif dealer_tot > BUST_POINT || (player_tot < BUST_POINT && dealer_tot < player_tot)
-    return "Player"
+  if player_tot > BUST_POINT
+    'player busted'
+  elsif dealer_tot > BUST_POINT
+    'dealer busted'
+  elsif dealer_tot < player_tot
+    'Player'
+  elsif player_tot < dealer_tot
+    'Dealer'
+  else
+    'tie'
   end
 end
 
-def display_winner(winner)
-  puts "#{winner} wins the game!"
+def display_winner(player_tot, dealer_tot)
+  result = check_winner(player_tot, dealer_tot)
+
+  case result
+  when 'player busted'
+    puts 'You busted! Dealer wins!'
+  when 'dealer busted'
+    puts 'Dealer busted! You win!'
+  when 'Player'
+    puts 'You win!'
+  when 'Dealer'
+    puts 'Dealer wins!'
+  when 'tie'
+    puts "It's a tie"
+  end
 end
 
-def display_player_hand(player_hand)
+# def display_player_hand(player_hand)
+# end
+#
+# def display_dealer_hand(dealer_hand)
+# end
 
+def play_again?
+  choice = ''
+  loop do
+    puts 'Do you want to play again? y/n '
+    choice = gets.chomp.downcase
+    break if %w(y n).include?(choice)
+    puts "invalid response. enter 'y' or 'n'"
+  end
+  choice == 'y'
 end
 
-def display_dealer_hand(dealer_hand)
+def update_score(hsh, string)
+  hsh[string] += 1
+end
 
+def grand_winner?(hsh)
+  hsh.values.any? { |e| e == 5 }
 end
 
 # Game Play
 
 puts "Welcome to the game of 21!"
-
+score_total = Hash.new(0)
 loop do
+  deck = initialize_deck
 
   puts "...issuing two cards to player"
   player_hand = deck.sample(2)
@@ -81,12 +124,18 @@ loop do
   puts "You have: #{player_hand[0][1]} and #{player_hand[1][1]}"
 
   loop do
-    print "Enter 'h' to hit or 's' to stay: "
-    player_choice = gets.chomp
+    player_choice = ''
+    loop do
+      print "Enter 'h' to hit or 's' to stay: "
+      player_choice = gets.chomp.downcase
+      break if %w(h s).include?(player_choice)
+      puts "invalid choice, please enter 'h' or 's'"
+    end
 
     if player_choice == 'h'
-      player_hand = player_hand + [deck.sample]
-      p player_hand
+      player_hand += [deck.sample]
+      puts 'You chose to hit'
+      puts "Your cards are now #{player_hand}"
       update_deck_draw(deck, player_hand)
       player_total = update_total(player_hand)
     end
@@ -94,20 +143,53 @@ loop do
     break if busted?(player_total) || player_choice == 's'
   end
 
-  while dealer_total < DEALER_HEDGE do
-    dealer_hand = dealer_hand + [deck.sample]
+  if busted?(player_total)
+    update_score(score_total, 'Player')
+    display_winner(player_total, dealer_total)
+    play_again? ? next : break
+  else
+    puts 'You chose to stay!'
+    puts "Your current total is #{player_total}"
+  end
+
+  puts 'Dealer turn...'
+
+  while dealer_total < DEALER_HEDGE
+    puts 'Dealer hits!'
+    dealer_hand += [deck.sample]
+    puts "Dealer's cards are now #{dealer_hand}"
     update_deck_draw(deck, dealer_hand)
     dealer_total = update_total(dealer_hand)
 
     break if busted?(dealer_total)
   end
 
-  winner = check_winner(player_total, dealer_total)
-  display_winner(winner)
+  if busted?(dealer_total)
+    update_score(score_total, 'Dealer')
+    puts "Dealer total is now: #{dealer_total}"
+    display_winner(player_total, dealer_total)
+    play_again? ? next : break
+  else
+    puts "Dealer stays at #{dealer_total}"
+  end
 
-  puts "Do you want to play again? y/n "
-  choice = gets.chomp
-  break if choice == 'n'
+  # result time
+  puts '======================='
+  puts "Dealer has #{dealer_hand}, for a total of: #{dealer_total}"
+  puts "Player has #{player_hand}, for a total of: #{player_total}"
+  puts '======================='
+
+  result = check_winner(player_total, dealer_total)
+  update_score(score_total, result) if %w(Player Dealer).include?(result)
+
+  display_winner(player_total, dealer_total)
+
+  if grand_winner?(score_total)
+    puts "Game over! #{score_total.key(5)} has won 5 games!"
+    break
+  end
+
+  break unless play_again?
 end
 
 puts "Thank you for playing 21!"
