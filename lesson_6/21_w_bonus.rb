7907fde5c4b5832e1cefa903f10e0c1f4fbce81e
Chijioke Onyekwelu <coonyekwelu@gmail.com>
2019-08-31 14:11:17 -0700

    21

--- lesson_6/21_w_bonus.rb
+++ lesson_6/21_w_bonus.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # 1. Initialize deck
 # 2. Deal cards to player and dealer
 # update deck
@@ -18,6 +19,7 @@ VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 BUST_POINT = 21
 DEALER_HEDGE = 17
+WAIT_TIME = 1.5
 
 def initialize_deck
   SUITS.product(VALUES).shuffle
@@ -98,98 +100,120 @@ def update_score(hsh, string)
   hsh[string] += 1
 end
 
-def grand_winner?(hsh)
-  hsh.values.any? { |e| e == 5 }
+def grand_winner?(player_score, dealer_score)
+  player_score == 5 || dealer_score == 5
 end
 
+def display_grand_winner(player_score, dealer_score)
+  if player_score == 5
+    puts "Game over! You have won 5 games!"
+  elsif dealer_score == 5
+    puts "Game over! Dealer has won 5 games!"
+  end
+end
+
+def request_player_choice
+  player_choice = ''
+  loop do
+    print "Enter 'h' to hit or 's' to stay: "
+    player_choice = gets.chomp.downcase
+    break if %w(h s).include?(player_choice)
+    puts "invalid choice, please enter 'h' or 's'"
+  end
+  player_choice
+end
 # Game Play
 
 puts "Welcome to the game of 21!"
-score_total = Hash.new(0)
 loop do
-  deck = initialize_deck
-
-  puts "...issuing two cards to player"
-  player_hand = deck.sample(2)
-  update_deck_draw(deck, player_hand)
-
-  puts "...issuing two cards to dealer"
-  dealer_hand = deck.sample(2)
-  update_deck_draw(deck, dealer_hand)
-
-  player_total = update_total(player_hand)
-  dealer_total = update_total(dealer_hand)
-
-  puts "Dealer has: #{dealer_hand.sample[1]} and unknown card"
-  puts "You have: #{player_hand[0][1]} and #{player_hand[1][1]}"
+  player_score = 0
+  dealer_score = 0
 
   loop do
-    player_choice = ''
-    loop do
-      print "Enter 'h' to hit or 's' to stay: "
-      player_choice = gets.chomp.downcase
-      break if %w(h s).include?(player_choice)
-      puts "invalid choice, please enter 'h' or 's'"
-    end
+    deck = initialize_deck
+    system ("cls")
 
-    if player_choice == 'h'
-      player_hand += [deck.sample]
-      puts 'You chose to hit'
-      puts "Your cards are now #{player_hand}"
-      update_deck_draw(deck, player_hand)
-      player_total = update_total(player_hand)
-    end
+    puts "...issuing two cards to player"
+    sleep WAIT_TIME
+    player_hand = deck.sample(2)
+    update_deck_draw(deck, player_hand)
 
-    break if busted?(player_total) || player_choice == 's'
-  end
-
-  if busted?(player_total)
-    update_score(score_total, 'Player')
-    display_winner(player_total, dealer_total)
-    play_again? ? next : break
-  else
-    puts 'You chose to stay!'
-    puts "Your current total is #{player_total}"
-  end
-
-  puts 'Dealer turn...'
-
-  while dealer_total < DEALER_HEDGE
-    puts 'Dealer hits!'
-    dealer_hand += [deck.sample]
-    puts "Dealer's cards are now #{dealer_hand}"
+    puts "...issuing two cards to dealer"
+    sleep WAIT_TIME
+    dealer_hand = deck.sample(2)
     update_deck_draw(deck, dealer_hand)
+
+    player_total = update_total(player_hand)
     dealer_total = update_total(dealer_hand)
 
-    break if busted?(dealer_total)
-  end
+    puts "Dealer has: #{dealer_hand.sample[1]} and unknown card"
+    puts "You have: #{player_hand[0][1]} and #{player_hand[1][1]}"
 
-  if busted?(dealer_total)
-    update_score(score_total, 'Dealer')
-    puts "Dealer total is now: #{dealer_total}"
+    loop do
+      player_choice = request_player_choice
+
+      if player_choice == 'h'
+        player_hand += [deck.sample]
+        puts 'You chose to hit'
+        puts "Your cards are now #{player_hand}"
+        update_deck_draw(deck, player_hand)
+        player_total = update_total(player_hand)
+      end
+
+      break if busted?(player_total) || player_choice == 's'
+    end
+
+    if busted?(player_total)
+      dealer_score += 1
+      display_winner(player_total, dealer_total)
+      break
+    else
+      puts 'You chose to stay!'
+      puts "Your current total is #{player_total}"
+    end
+
+    puts 'Dealer turn...'
+    sleep WAIT_TIME
+
+    while dealer_total < DEALER_HEDGE
+      puts 'Dealer hits!'
+      dealer_hand += [deck.sample]
+      puts "Dealer's cards are now #{dealer_hand}"
+      update_deck_draw(deck, dealer_hand)
+      dealer_total = update_total(dealer_hand)
+
+      break if busted?(dealer_total)
+    end
+
+    if busted?(dealer_total)
+      player_score += 1
+      puts "Dealer total is now: #{dealer_total}"
+      display_winner(player_total, dealer_total)
+      break
+    else
+      puts "Dealer stays at #{dealer_total}"
+    end
+
+    # result time
+    puts '======================='
+    puts "Dealer has #{dealer_hand}, for a total of: #{dealer_total}"
+    puts "Player has #{player_hand}, for a total of: #{player_total}"
+    puts '======================='
+
+    result = check_winner(player_total, dealer_total)
+    player_score += 1 if result == "Player"
+    dealer_score += 1 if result == "Dealer"
+
     display_winner(player_total, dealer_total)
-    play_again? ? next : break
-  else
-    puts "Dealer stays at #{dealer_total}"
-  end
 
-  # result time
-  puts '======================='
-  puts "Dealer has #{dealer_hand}, for a total of: #{dealer_total}"
-  puts "Player has #{player_hand}, for a total of: #{player_total}"
-  puts '======================='
+    if grand_winner?(player_score, dealer_score)
+      display_grand_winner(player_score, dealer_score)
+    end
 
-  result = check_winner(player_total, dealer_total)
-  update_score(score_total, result) if %w(Player Dealer).include?(result)
-
-  display_winner(player_total, dealer_total)
-
-  if grand_winner?(score_total)
-    puts "Game over! #{score_total.key(5)} has won 5 games!"
     break
   end
-
+  binding.pry
   break unless play_again?
 end
 
-puts "Thank you for playing 21!"
+puts "Thank you for playing #{BUST_POINT}!"
